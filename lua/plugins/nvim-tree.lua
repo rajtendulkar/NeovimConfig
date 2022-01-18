949cabc4b8f9b66e1d336383e03f2304dda4df79
Pranav Tendulkar <pranav.tendulkar@intel.com>
2022-01-18 11:53:20 +0100

    update NVIM-tree parameters
    Signed-off-by: Pranav Tendulkar <pranav.tendulkar@intel.com>

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -7,16 +7,12 @@ local tree_cb = require"nvim-tree.config".nvim_tree_callback
 -- Set alias for vim.g.
 local g = vim.g
 
-g.nvim_tree_ignore = { ".git", ".ccls-cache", ".cache", "__pycache__"} -- Ignore these types in listing.
 g.nvim_tree_auto_ignore_ft = { "dashboard" } -- Don't open tree on specific fiypes.
 --g.nvim_tree_quit_on_open = 1 -- closes tree when file's opened.
-g.nvim_tree_tab_open = 0 -- file opens on Tab key.
 g.nvim_tree_indent_markers = 1 -- This option shows indent markers when folders are open.
-g.nvim_tree_hide_dotfiles = 1 -- This option hides files and folders starting with a dot `.`.
 g.nvim_tree_git_hl = 1 -- Will enable file highlight for git attributes (can be used without the icons).
 g.nvim_tree_highlight_opened_files = 0 -- Will enable folder and file icon highlight for opened files/directories.
 g.nvim_tree_add_trailing = 0 -- Append a trailing slash to folder names. ]]
-g.nvim_tree_disable_default_keybindings = 1 -- Disables default key bindings
 vim.g.nvim_tree_window_picker_exclude = {
     filetype = {'packer', 'vista_kind'},
     buftype = {'terminal'}
@@ -31,13 +27,14 @@ nvimtree.setup {
     auto_open = true,
   },
   filters = {
-    dotfiles = false,
-    custom = {".git", ".github", ".clang-format", ".gitignore", ".gitmodules"}
+    dotfiles = true,
+    custom = {".git", ".github", ".clang-format", ".gitignore", ".gitmodules", "__pycache__", ".cache", ".ccls-cache"}
   },
   view = {
     width = "15%",
     side = "left",
     mappings = {
+      custom_only = true,
       list = {
        { key = { '<leftmouse>' }, cb = tree_cb("edit") },
        {key = "<S-h>", cb = ":call ResizeLeft(3)<CR>"},
